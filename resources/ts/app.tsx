3fb656877f08cebfc68cc83d40d6d7851b842b45
Tim Schalk <email@timschalk.de>
2023-03-07 09:51:11 +0100

    first commit

---
+++ resources/ts/app.tsx
@@ -0,0 +1,17 @@
+import "./bootstrap";
+import "../css/app.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/inertia-react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+
+createInertiaApp({
+  resolve: (name) =>
+    resolvePageComponent(
+      `./Pages/${name}.tsx`,
+      import.meta.glob("./Pages/**/*.tsx")
+    ),
+  setup({ el, App, props }) {
+    createRoot(el).render(<App {...props} />);
+  },
+});
